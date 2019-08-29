346f1b45a1cd3089837916fb28744975ca6044df
Casomniac <Casomniac@gmail.com>
2019-08-29 12:38:18 +0500

    Add try-catch

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const getMinSum = (arr) => {
-let min = Math.min.apply(null, arr);
+    try {
+        let min = Math.min.apply(null, arr);
 
-if (min === Infinity || isNaN(min)) return new Error('Массив либо пуст, либо не содержит цифр!!!');
+        if (min === Infinity || isNaN(min)) throw new Error('Массив либо пуст, либо не содержит цифр!!!');
 
-let secondMin = Math.min.apply(null, arr.filter(num => num !== min));
-return min + secondMin;
+        let secondMin = Math.min.apply(null, arr.filter(num => num !== min));
+        return min + secondMin;
+    } catch (err) {
+        return (err.message)
+    }
+
 };
 
 getMinSum([4, 0, 3, 19, 492, -10, 1]); // should return -10
