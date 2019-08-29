c640d918b861cf265b9659d0a277a9fe107126ec
Casomniac <Casomniac@gmail.com>
2019-08-29 18:54:41 +0500

    Edit code

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const getMinSum = (arr) => {
-    try {
-        let min = Math.min.apply(null, arr);
+  let numArr = arr.filter(num => typeof(num) !== 'string'),
+    firstMin,
+    secondMin;
 
-        if (min === Infinity || isNaN(min)) throw new Error('Массив либо пуст, либо не содержит цифр!!!');
+  if (numArr.length >= 2) {
+    if (numArr[0] > numArr[1]) {
+        firstMin = numArr[1];
+        secondMin = numArr[0]
 
-        let secondMin = Math.min.apply(null, arr.filter(num => num !== min));
-        return min + secondMin;
-    } catch (err) {
-        return (err.message)
+    } else {
+        firstMin = numArr[0];
+        secondMin = numArr[1]
     }
 
+    for (let i = 2; i < numArr.length; i++) {
+      if (numArr[i] <= firstMin) {
+          secondMin = firstMin;
+          firstMin = numArr[i]
+
+      } else if (numArr[i] < secondMin){
+          secondMin = numArr[i]
+      }
+    }
+  } else return 'Массив либо пустой, либо содержит один элемент, либо не содержит цифр';
+
+  return secondMin + firstMin
 };
 
 getMinSum([4, 0, 3, 19, 492, -10, 1]); // should return -10
 getMinSum(['gdfgdfg', 'gfdhytjyjy', 'hgfhfgh']); // should return error
 getMinSum([]); // should return error
+getMinSum([4]); // should return error
