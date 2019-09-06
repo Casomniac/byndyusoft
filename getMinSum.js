59a26cc92122f8d89901f5c632a203019b740b68
Casomniac <Casomniac@gmail.com>
2019-09-06 11:53:48 +0500

    добавил исключения и проверку на null/undefined

--- getMinSum.js
+++ getMinSum.js
@@ -1,29 +1,31 @@
 const getMinSum = (arr) => {
-  let numArr = arr.filter(num => typeof(num) !== 'string'),
-    firstMin,
-    secondMin;
-
-  if (numArr.length >= 2) {
-    if (numArr[0] > numArr[1]) {
-        firstMin = numArr[1];
-        secondMin = numArr[0]
-
-    } else {
-        firstMin = numArr[0];
-        secondMin = numArr[1]
+    if (arr === undefined || arr === null) {
+        throw new Error('Массив либо null, либо undefined')
     }
+    let numArr = arr.filter(num => typeof (num) !== 'string'),
+        firstMin,
+        secondMin;
 
-    for (let i = 2; i < numArr.length; i++) {
-      if (numArr[i] <= firstMin) {
-          secondMin = firstMin;
-          firstMin = numArr[i]
-      } else if (numArr[i] < secondMin){
-          secondMin = numArr[i]
-      }
-    }
-  } else return 'Массив либо пустой, либо содержит один элемент, либо не содержит цифр';
+    if (numArr.length >= 2) {
+        if (numArr[0] > numArr[1]) {
+            firstMin = numArr[1];
+            secondMin = numArr[0]
 
-  return secondMin + firstMin
+        } else {
+            firstMin = numArr[0];
+            secondMin = numArr[1]
+        }
+
+        for (let i = 2; i < numArr.length; i++) {
+            if (numArr[i] <= firstMin) {
+                secondMin = firstMin;
+                firstMin = numArr[i]
+            } else if (numArr[i] < secondMin) {
+                secondMin = numArr[i]
+            }
+        }
+    } else throw new Error('Массив либо пустой, либо содержит один элемент, либо не содержит цифр');
+
+    return secondMin + firstMin
 };
-
 module.exports = getMinSum;
